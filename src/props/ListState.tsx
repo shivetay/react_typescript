20fa64bdbf7e1c24eff5cd046ae7bfe6f27f5858
Łukasz Dawidowicz <lddev@outlook.com>
2021-07-09 14:48:32 +0200

    work with state

--- src/props/ListState.tsx
+++ src/props/ListState.tsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 
 
 const ListState: React.FC = () => {
+  const [name,setName] = useState('')
+  const [guests, setGuests] = useState<string[]>([])
+
+  const onClick = () =>{
+setName('')
+setGuests([...guests, name])
+  }
   return (
     <div>
       <h1>Guest list</h1>
+<ul>
+{guests.map(
+  (guest)=><li key={guest}>{guest}</li>
+
+
+)}
+</ul>
+      <input value={name} onChange={(e) => setName(e.target.value)}/>
+      <button onClick={onClick}>Add guest</button>
     </div>
   )
 }
