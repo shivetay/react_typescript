a2b8edff27cfaf9a75a3e5e348ac50f3b691c07f
Łukasz Dawidowicz <lddev@outlook.com>
2021-07-19 11:27:55 +0200

    init repo reducer

---
+++ src/redux/repoReducer.ts
@@ -0,0 +1,33 @@
+interface RepoState {
+  loading: boolean;
+  error: string | null;
+  data: string[];
+}
+
+const reducer = (state: RepoState, action: any) => {
+  switch (action.type) {
+    case 'SEARCH_REPO':
+      return {
+        loading: true,
+        error: null,
+        data: [],
+      };
+    case 'SEARCH_REPO_ERROR':
+      return {
+        loading: false,
+        error: action.payload,
+        data: [],
+      };
+    case 'SEARCH_REPO_SUCCESS':
+      return {
+        loading: false,
+        error: null,
+        data: action.payload,
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
